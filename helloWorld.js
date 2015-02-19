81814713df7b3f29083e2216b98a6bf1d80f9979
Jamie Wilson <jamie.wilsons@gmail.com>
2015-02-19 13:36:25 -0800

    first-code-commit

---
+++ helloWorld.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function(event, done) {
+
+  return 
+    Hoist.log("Hello World! Congratulations, you've just fired your first Hoist event.")
+    .then(function() {
+      return Hoist.log("To keep learning checkout out our tutorial at hoist.io/tutorial");
+    })
+    .then(function() {
+      return Hoist.log("Get this apps source code at github.com/hoist/hoist-sample-app");
+    })
+    .then(done);
+
+};
