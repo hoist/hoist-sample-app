0f40652ea8544c6f1e862e03c7a1e87f6f255ff2
Jamie Wilson <jamie.wilsons@gmail.com>
2015-02-19 13:44:28 -0800

    test

--- helloWorld.js
+++ helloWorld.js
@@ -4,12 +4,13 @@ module.exports = function(event, done) {
 
   return 
     Hoist.log("Hello World! Congratulations, you've just fired your first Hoist event.")
-    .then(function() {
-      return Hoist.log("To keep learning checkout out our tutorial at hoist.io/tutorial");
-    })
-    .then(function() {
-      return Hoist.log("Get this apps source code at github.com/hoist/hoist-sample-app");
-    })
     .then(done);
+    // .then(function() {
+    //   return Hoist.log("To keep learning checkout out our tutorial at hoist.io/tutorial");
+    // })
+    // .then(function() {
+    //   return Hoist.log("Get this apps source code at github.com/hoist/hoist-sample-app");
+    // })
+
 
 };
